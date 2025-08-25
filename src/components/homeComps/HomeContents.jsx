456096c58b0bfa2b6ef8f5ada73a98000f62694e
Paidlife8 <Paidlife8@gmail.com>
2025-08-25 08:22:19 -0700

    newupdate

--- src/components/homeComps/HomeContents.jsx
+++ src/components/homeComps/HomeContents.jsx
@@ -138,11 +138,10 @@ const HomeContents = () => {
                     <i className="icon-5" />
                   </div>
                   <h4>
-                    <a href="#">Secure International Transaction</a>
+                    <a href="#">Innovation at Scale</a>
                   </h4>
                   <p>
-                    Tortor neque sed tellus estian eget dui id ante tristique
-                    more tristique dolor.
+                    Cutting-edge technology that simplifies money management, anytime and anywhere.
                   </p>
                 </div>
               </div>
@@ -152,11 +151,10 @@ const HomeContents = () => {
                     <i className="icon-6" />
                   </div>
                   <h4>
-                    <a href="#">24/7 Support from the Expert Team</a>
+                    <a href="#">24/7 Personalized Support from the Expert Team</a>
                   </h4>
                   <p>
-                    Tortor neque sed tellus estian eget dui id ante tristique
-                    more tristique dolor.
+                    Dedicated teams offering guidance tailored to your financial goals.
                   </p>
                 </div>
               </div>
@@ -166,11 +164,10 @@ const HomeContents = () => {
                     <i className="icon-7" />
                   </div>
                   <h4>
-                    <a href="#">Lowest Processing Fee than Other Banks</a>
+                    <a href="#">Community-Driven Values</a>
                   </h4>
                   <p>
-                    Tortor neque sed tellus estian eget dui id ante tristique
-                    more tristique dolor.
+                    A commitment to integrity, trust, and financial growth for every member.
                   </p>
                 </div>
               </div>
@@ -180,11 +177,10 @@ const HomeContents = () => {
                     <i className="icon-8" />
                   </div>
                   <h4>
-                    <a href="#">Less Time in any Loans Approval</a>
+                    <a href="#">Global Presence</a>
                   </h4>
                   <p>
-                    Tortor neque sed tellus estian eget dui id ante tristique
-                    more tristique dolor.
+                    Serving individuals and businesses across the United Sates, Europe and Asia with Seamless cross-border solutions.
                   </p>
                 </div>
               </div>
